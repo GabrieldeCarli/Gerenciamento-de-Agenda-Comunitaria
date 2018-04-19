67009ee468a7cf03b66eda7ab398b16b0aa5dce0
GabrieldeCarli <32376942+GabrieldeCarli@users.noreply.github.com>
2018-04-19 10:01:11 -0300

    Login anexado
    Nao funciona AINDA

---
+++ AgendaComunitária/src/java/Logar.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author gabri
+ */
+@WebServlet(urlPatterns = {"/Logar"})
+public class Logar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Veja os eventos</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<fieldset>");
+            out.println("<legend>LOGAR</legend>");
+            out.println("<form action=\"logar\" method=\"post\">");
+            out.println("Nome: <input type=\"text\" name=\"name_user\" /><br />");
+            out.println("Senha: <input type=\"password\" name=\"senha\" /><br />");
+            out.println("<input type=\"submit\" value=\"Login\" />");
+            out.println("</form>");
+            out.println("</fieldset>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        String name_user = request.getParameter("name_user");
+        String senha = request.getParameter("senha");
+        List<Usuario> user = new ArrayList();
+        
+        try {
+            
+            UsuarioDAO usuario = new UsuarioDAO();
+            user = usuario.validar(name_user, senha);
+            
+            if (user == null) {
+                out.println("<html><body>Usuário ou senha inválida</body></html>");
+            } else {
+                HttpSession session = request.getSession();
+                session.setAttribute("usuario.logado", usuario);
+                out.println("<html><body>Usuário logado: " + name_user + "</body></html>");
+
+                response.sendRedirect("/Eventos");
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Logar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- AgendaComunitária/src/java/UsuarioDAO.java
+++ AgendaComunitária/src/java/UsuarioDAO.java
@@ -12,29 +12,29 @@ import java.util.List;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author luiz-
  */
 public class UsuarioDAO {
+
     private Connection connection;
-    
-    public UsuarioDAO() throws ClassNotFoundException{
+
+    public UsuarioDAO() throws ClassNotFoundException {
         this.connection = new ConnectionFactory().getConnection();
     }
-    
-    public void adicionaUsuario(Usuario usuario){
-        String sql = "INSERT INTO usuario (name_user, password)" 
+
+    public void adicionaUsuario(Usuario usuario) {
+        String sql = "INSERT INTO usuario (name_user, password)"
                 + " VALUES (?, ?)";
-        
+
         try {
             // prepared statement para inserção
             PreparedStatement stmt = (PreparedStatement) connection.prepareStatement(sql);
 
             // seta os valores
-            stmt.setString(1,usuario.getUserName());
-            stmt.setString(2,usuario.getPassword());
+            stmt.setString(1, usuario.getUserName());
+            stmt.setString(2, usuario.getPassword());
 
             // executa a query
             stmt.execute();
@@ -43,22 +43,46 @@ public class UsuarioDAO {
             throw new RuntimeException(e);
         }
     }
-    
-     public List<Usuario> getListaUsuarios(){
+
+    public List<Usuario> getListaUsuarios() {
         try {
             List<Usuario> contatos = new ArrayList<Usuario>();
             PreparedStatement ps = (PreparedStatement) this.connection.prepareStatement("select * from usuario");
-            
+
             ResultSet rs = ps.executeQuery();
-            
-            while (rs.next()){
-                Usuario user = new Usuario();                
-                user.setUserName(rs.getString("name_user"));                
+
+            while (rs.next()) {
+                Usuario user = new Usuario();
+                user.setUserName(rs.getString("name_user"));
                 contatos.add(user);
             }
             rs.close();
             ps.close();
             return contatos;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Usuario> validar(String name_user, String password) {
+        String sql = "select name_user, password from usuario WHERE name_user LIKE ? AND password ?";
+
+        try {
+            List<Usuario> user = new ArrayList();
+            java.sql.PreparedStatement ps = (java.sql.PreparedStatement) this.connection.prepareStatement(sql);
+            ps.setString(1, name_user);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setUserName(rs.getString("name_user"));
+                usuario.setUserName(rs.getString("password"));
+                usuario.setId(rs.getLong("id_user"));
+                user.add(usuario);
+            }
+            ps.close();
+            return user;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
